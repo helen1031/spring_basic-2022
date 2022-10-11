82ec30fa92a9e9d1d7ee7b0e416b81b4ea497958
helen1031 <32950301+helen1031@users.noreply.github.com>
2022-10-12 04:01:39 +0900

    initial commit

---
+++ src/main/java/spring_basic/spring_basic2022/SpringBasic2022Application.java
@@ -0,0 +1,13 @@
+package spring_basic.spring_basic2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasic2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasic2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/spring_basic/spring_basic2022/SpringBasic2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_basic.spring_basic2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasic2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
